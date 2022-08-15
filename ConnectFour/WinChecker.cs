64ef09c50b2daab06c5c797b5ece05a9d359b81a
Kevin Kraemer <kraemer.kevin747@gmail.com>
2022-08-15 21:36:11 +0200

    (feat) Add Triangular Winchecker

--- ConnectFour/WinChecker.cs
+++ ConnectFour/WinChecker.cs
@@ -7,16 +7,19 @@ public class WinChecker
     private ConnectFourBoard _board;
     public bool IsOver;
     public IPlayer? Winner;
-    public WinChecker(ConnectFourBoard board)
+    public WinChecker(Game game)
     {
-        _board = board;
+        _board = game.Board;
     }
 
     public void Check(IPlayer player)
     {
+        ConsoleColor previousColor = Console.ForegroundColor;
         _playerColor = player.Color.ToConsoleColor();
         CheckColumns(player);
         CheckRows(player);
+        CheckDiagonals(player);
+        _playerColor = previousColor;
     }
 
     private void CheckColumns(IPlayer player)
@@ -77,6 +80,91 @@ public class WinChecker
         }
     }
 
+    private void CheckDiagonals(IPlayer player)
+    {
+        _playerColor = player.Color.ToConsoleColor();
+
+        CheckLowerTriangular(player);
+        CheckUpperTriangular(player);
+        
+        //CheckAboveAntiDiagonal(player);
+        //CheckBelowAntiDiagonal(player);
+    }
+
+    private void CheckLowerTriangular(IPlayer player)
+    {
+        int midRows = _board.Rows / 2;
+        for (int diagonal = 0; diagonal < midRows; diagonal++)
+        {
+            int countConsecutiveCoins = 0;
+            for (int row = 0; row < _board.Rows; row++)
+            {
+                for (int col = 0; col < _board.Columns - 1; col++)
+                {
+                    if (row != col) continue;
+                    
+                    int selectedRow = diagonal + row;
+                    if (selectedRow > _board.Rows - 1) break;
+
+                    var cell = _board.Grid[selectedRow, col];
+                    if (cell.Cell == Cell.Empty) break;
+
+                    if (cell.Color != _playerColor)
+                    {
+                        countConsecutiveCoins = 0;
+                        continue;
+                    }
+
+                    countConsecutiveCoins++;
+                    Console.WriteLine($"CheckLowerTriangular Diagonal #{diagonal} | Grid[row: {selectedRow}, col: {col}], Count: {countConsecutiveCoins}");
+                    DetermineWinner(player, countConsecutiveCoins);
+                }
+            } 
+        }
+    }
+
+    private void CheckUpperTriangular(IPlayer player)
+    {
+        int midCols = _board.Columns / 2;
+        for (int diagonal = 1; diagonal <= midCols; diagonal++)
+        {
+            int countConsecutiveCoins = 0;
+            for (int row = 0; row < _board.Rows; row++)
+            {
+                for (int col = 0; col < _board.Columns; col++)
+                {
+                    if (row != col) continue;
+                    
+                    int selectedCol = diagonal + col;
+                    if (selectedCol > _board.Columns - 1) break;
+
+                    var cell = _board.Grid[row, selectedCol];
+                    if (cell.Cell == Cell.Empty) break;
+
+                    if (cell.Color != _playerColor)
+                    {
+                        countConsecutiveCoins = 0;
+                        continue;
+                    }
+
+                    countConsecutiveCoins++;
+                    Console.WriteLine($"CheckUpperTriangular Diagonal #{diagonal} | Grid[row: {row}, col: {selectedCol}], Count: {countConsecutiveCoins}");
+                    DetermineWinner(player, countConsecutiveCoins);
+                }
+            }
+        }
+    }
+
+    private void CheckAboveAntiDiagonal(IPlayer player)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void CheckBelowAntiDiagonal(IPlayer player)
+    {
+        throw new NotImplementedException();
+    }
+
     private void DetermineWinner(IPlayer player, int countConsecutiveCoins)
     {
         if (countConsecutiveCoins == ConnectFour)
@@ -86,3 +174,4 @@ public class WinChecker
         }
     }
 }
+
