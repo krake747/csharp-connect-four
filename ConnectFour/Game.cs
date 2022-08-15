bfb4a3aa96ad07537c5520bfbe90132c2cd9cccf
Kevin Kraemer <kraemer.kevin747@gmail.com>
2022-08-15 21:36:40 +0200

    (feat) Add event to Console rendering

--- ConnectFour/Game.cs
+++ ConnectFour/Game.cs
@@ -4,15 +4,18 @@ public class Game
 {
     private readonly Renderer _renderer = new Renderer();
     private readonly WinChecker _winChecker;
+    private int _turnCount;
     public IPlayer Player1 { get; }
     public IPlayer Player2 { get; }
     public ConnectFourBoard Board { get; } = new ConnectFourBoard();
+    public event Action DisplayBoard;
     public bool IsOver => _winChecker.IsOver;
     public Game(IPlayer player1, IPlayer player2)
     {
         Player1 = player1;
         Player2 = player2;
-        _winChecker = new WinChecker(Board);
+        _winChecker = new WinChecker(this);
+        DisplayBoard += RenderBoard;
     }
     
     public void Run()
@@ -34,11 +37,18 @@ public class Game
         foreach (var player in group)
         {
             player.ChooseAction(this, player).Run(this, player);
-            _renderer.Render(this);
+            DisplayBoard.Invoke();
             _winChecker.Check(player);
 
             if (IsOver) break;
         }
+    }
+
+    private void RenderBoard() 
+    {
+        Console.Clear();
+        ColoredConsole.WriteInfo($"Turn #{++_turnCount}");
+        _renderer.Render(this);
     }
 
     private void GreetingMessage() => ColoredConsole.WriteInfo($"Welcome {Player1.Name}, {Player2.Name} to Connect Four!");
